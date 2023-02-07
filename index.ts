140091398437a27c278aee2f86c7b2cbc3c8711e
siu <siu.issiki@gmail.com>
2023-02-08 00:29:02 +0900

    add copy text

--- index.ts
+++ index.ts
@@ -43,7 +43,21 @@ router.post("/slack_event", async (ctx) => {
   const {
     event: { text },
   } = await ctx.request.body({ type: "json" }).value;
-  console.log(text);
+
+  const url =
+    "https://hooks.slack.com/services/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";
+  const body = {
+    text,
+  };
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers,
+  };
+  await fetch(url, options);
 
   ctx.response.body = { text };
 });
