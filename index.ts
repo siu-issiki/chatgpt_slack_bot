87a186f0884e67e0a0aac880bbdd6c857b2717b8
siu <siu.issiki@gmail.com>
2023-02-07 23:45:12 +0900

    add openapi call router

--- index.ts
+++ index.ts
@@ -1,3 +1,45 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-serve((req: Request) => new Response("Hello World"));
+const app = new Application();
+const router = new Router();
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  console.log(
+    `Listening on: ${secure ? "https://" : "http://"}${
+      hostname ?? "localhost"
+    }:${port}`
+  );
+});
+
+app.addEventListener("error", (evt) => {
+  console.log(evt.error);
+});
+
+const url = "https://api.openai.com/v1/completions";
+const apiKey = Deno.env.get("OPENAI_API_KEY");
+router.get("/", async (ctx) => {
+  const prompt = "健康に良い習慣を教えてください";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+  const body = {
+    model: "text-davinci-003",
+    max_tokens: 1024,
+    temperature: 0.9,
+    prompt,
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers,
+  };
+  const res = await fetch(url, options);
+
+  ctx.response.body = await res.json();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8080 });
